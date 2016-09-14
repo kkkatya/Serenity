e50ec0242e502907b63d195fd479538aa52c6238
Maksim Paderin <Maksim Paderin>
2016-09-14 20:47:04 +0300

    Front-end

---
+++ resources/views/index.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Maksim & Kate Portfolio Page</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/full-width-pics.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="">Serenity</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#aboutus">About</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contacts">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <section id="hello">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12" id="hi">
+                    <h1 class="section-heading">We are Serenity</h1>
+                    <p>Web developers</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Full Width Image Header with Logo -->
+    <!-- Image backgrounds are set within the full-width-pics.css file. -->
+ <!--   <header class="image-bg-fluid-height">
+        <img class="img-responsive img-center" src="http://placehold.it/200x200&text=Logo" alt="">
+  </header> -->
+
+    <!-- Content Section -->
+    <section id="aboutus">
+        <div class="container relativebox">
+            <div class="row">
+                <div class="col-lg-12">
+                <h1 class="section-heading">About Us</h1>
+                    <div class="leftside">
+                        <div class="infoblock" id="who">
+                        <h3>Who we are?</h3>
+                        <p> We are two students from Finland developing websites. </p>
+                        </div>
+                        <div class="infoblock" id="competencem">
+                        <h3>Maksim</h3>
+                        <p> Front-end - 90% <br> Photoshop - 80% <br> C#/Java - 60% <br> PHP - 60% <br> JavaScript - 50% <br> <br> Russian - native <br> English - fluent <br> Swedish - pre-intermediate</p>
+                        </div>
+                        <div class="infoblock" id="competencek">
+                        <h3>Kate</h3>
+                        <p> Back-end - 90% <br> Laravel - 70% <br> Design - 70% <br> Ruby on Rails - 70% <br> JavaScript - 60% <br> <br> Russian - native <br> English - fluent <br> Swedish - basic</p>
+                        </div>
+                    </div>
+                    <div class="rightside">
+                        <div class="infoblock" id="what">
+                        <h3>What we do?</h3>
+                        <p> We develop modern websites either based on any needed template or from scratch. We are always open for discussion.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="section-heading">Portfolio</h1>
+                </div>
+                <a href="http://uma-graphics.com">
+                <div class="col-lg-2 circle col-md-2 col-sm-3" id="uma">
+                </div>
+                </a>
+                <div class="col-lg-2 col-lg-offset-3 col-md-2 col-md-offset-3 circle">
+                    <p>UmaGraphics</p>
+                </div>
+                <div class="col-lg-2 col-lg-offset-3 col-md-2 col-md-offset-3 circle">
+                    <p>UmaGraphics</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <section id="contacts">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 contactname">
+                <i class="fa fa-envelope-o fa-4x" aria-hidden="true"></i>
+                <h5>Email:</h5>
+                <p><a href="mailto:mvpaderin@gmail.com">mvpaderin@gmail.com</a></p>
+                <p><a href="mailto:mvpaderin@gmail.com">katya.lad@gmail.com</a></p>
+                </div>
+                <div class="col-lg-6 contactname">
+                <i class="fa fa-skype fa-4x" aria-hidden="true"></i></i>
+                <h5>Skype:</h5>
+                <p>zauvijek_moja</p>
+                <p>katya.lad</p>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="row veryfooter">
+                <div class="col-lg-12">
+                    <p>&copy; 2016 Serenity</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
